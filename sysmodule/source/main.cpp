1919c99b3475ffc03451cd2dbbf22919f1bf0b7c
znxDomain <znxDomain@gmail.com>
2021-02-23 23:01:02 -0500

    add logging

--- sysmodule/source/main.cpp
+++ sysmodule/source/main.cpp
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 // Include the main libnx system header, for Switch development
 #include <switch.h>
+
+#define ENABLE_LOGGING 1
 
 // Size of the inner heap (adjust as necessary).
 #define INNER_HEAP_SIZE 0x80000
@@ -39,12 +42,10 @@ void __appInit(void)
 {
     Result rc;
 
-    // Open a service manager session.
     rc = smInitialize();
     if (R_FAILED(rc))
         fatalThrow(rc);
 
-    // Retrieve the current version of Horizon OS.
     rc = setsysInitialize();
     if (R_SUCCEEDED(rc)) {
         SetSysFirmwareVersion fw;
@@ -54,15 +55,19 @@ void __appInit(void)
         setsysExit();
     }
 
-    // Enable this if you want to use HID.
     rc = hidInitialize();
     if (R_FAILED(rc))
         fatalThrow(rc);
 
-    // Enable this if you want to use time.
     rc = timeInitialize();
     if (R_FAILED(rc))
         fatalThrow(rc);
+
+    rc = fsInitialize();
+    if (R_FAILED(rc))
+        diagAbortWithResult(MAKERESULT(Module_Libnx, LibnxError_InitFail_FS));
+
+    fsdevMountSdmc();
 
     smExit();
 }
@@ -70,6 +75,8 @@ void __appInit(void)
 // Service deinitialization.
 void __appExit(void)
 {
+    fsdevUnmountAll(); // Disable this if you don't want to use the SD card filesystem.
+    fsExit(); // Disable this if you don't want to use the filesystem.
     timeExit(); // Enable this if you want to use time.
     hidExit(); // Enable this if you want to use HID.
 }
@@ -78,13 +85,30 @@ void __appExit(void)
 }
 #endif
 
+void LogLine(const char* fmt, ...)
+{
+
+    stdout = stderr = fopen("/controllersaver.log", "a");
+    va_list ap;
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+    fclose(stdout);
+}
+
 // Main program entrypoint
 int main(int argc, char* argv[])
 {
+    LogLine("Starting Sysmodule.\n");
 
     // Configure our supported input layout: all players with standard controller styles
     padConfigureInput(8, HidNpadStyleSet_NpadStandard);
-    
+    PadState pad;
+    padInitializeAny(&pad);
+    padUpdate(&pad);
+    u64 kDown = padGetButtonsDown(&pad);
+    LogLine("Configured input.\n");
+
     time_t currentTime = 0;
     time_t keyTime = 0;
     u32 timerSeconds = 30;
@@ -97,6 +121,7 @@ int main(int argc, char* argv[])
     // Main loop
     while (true)
     {
+        // LogLine("Started Loop.\n");
         svcSleepThread(1e+8L);
             
         Result rc = timeGetCurrentTime(TimeType_UserSystemClock, (u64*)&currentTime);
@@ -105,22 +130,35 @@ int main(int argc, char* argv[])
         }
 
         if (currentTime - keyTime > timerSeconds){
+            LogLine("Timer Met: %jd\n", (intmax_t)currentTime);
+
             Result rc = timeGetCurrentTime(TimeType_UserSystemClock, (u64*)&keyTime);
             if (R_FAILED(rc)) {
                 fatalThrow(rc);
             }
-            
+            LogLine("Disconnecting.\n");
             // Just disconnect them all
-            hidDisconnectNpad(HidNpadIdType_No1);
-            hidDisconnectNpad(HidNpadIdType_No2);
-            hidDisconnectNpad(HidNpadIdType_No3);
-            hidDisconnectNpad(HidNpadIdType_No4);
-            hidDisconnectNpad(HidNpadIdType_No5);
-            hidDisconnectNpad(HidNpadIdType_No6);
-            hidDisconnectNpad(HidNpadIdType_No7);
-            hidDisconnectNpad(HidNpadIdType_No8);
-            hidDisconnectNpad(HidNpadIdType_Other);
-            hidDisconnectNpad(HidNpadIdType_Handheld);
+            Result rc1 = hidDisconnectNpad(HidNpadIdType_No1);
+            if (true || R_FAILED(rc1)) {
+                LogLine("Error HidNpadIdType_No1:%u - %X", rc1, rc1);
+            }
+            Result rc2 = hidDisconnectNpad(HidNpadIdType_No2);
+            if (true || R_FAILED(rc2)) {
+                LogLine("Error HidNpadIdType_No2:%u - %X", rc2, rc2);
+            }
+            Result rc3 = hidDisconnectNpad(HidNpadIdType_No3);
+            if (true || R_FAILED(rc3)) {
+                LogLine("Error HidNpadIdType_No3:%u - %X", rc3, rc3);
+            }
+            Result rc4 = hidDisconnectNpad(HidNpadIdType_Other);
+            if (true || R_FAILED(rc4)) {
+                LogLine("Error HidNpadIdType_Other:%u - %X", rc4, rc4);
+            }
+            Result rc6 = hidDisconnectNpad(HidNpadIdType_Handheld);
+            if (true || R_FAILED(rc6)) {
+                LogLine("Error HidNpadIdType_Handheld:%u - %X", rc6, rc6);
+            }
+            LogLine("Disconnected.\n");
         }
     }
     return 0;
